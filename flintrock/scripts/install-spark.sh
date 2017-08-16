8806ed2520d8568c8ed4871673d3c56c939f595e
dm-tran <dm-tran@users.noreply.github.com>
2017-08-16 10:35:09 +0200

    fix: use level 'ERROR' for parquet logs for Spark 2.1.x (#4)

--- flintrock/scripts/install-spark.sh
+++ flintrock/scripts/install-spark.sh
@@ -35,3 +35,13 @@ mkdir "spark"
 # strip-components puts the files in the root of spark/
 tar xzf "$file" -C "spark" --strip-components=1
 rm "$file"
+
+# Because of https://issues.apache.org/jira/browse/SPARK-17993, Spark 2.1 displays very
+# verbose logs when reading parquet files, with the default log4j file.
+# This is fixed by SPARK-19219, which is released in Spark 2.2
+#
+# Workaround: if url contains "spark-2.1", use a copy of log4j.properties.template which
+# contains level "ERROR" for parquet logs (default file log4j-defaults.properties doesn't)
+if [[ $url == *"spark-2.1"* ]]; then
+  cp -p spark/conf/log4j.properties.template spark/conf/log4j.properties
+fi
