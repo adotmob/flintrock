66476e15d09c84ea446b3847f071c104fff96e07
Diego Mora Cespedes <diego.moracespedes@adotmob.com>
2018-05-09 16:56:17 +0200

    release v0.9.2

--- flintrock/__init__.py
+++ flintrock/__init__.py
@@ -1,2 +1,6 @@
 # See: https://packaging.python.org/en/latest/distributing/#standards-compliance-for-interoperability
+<<<<<<< HEAD
 __version__ = '1.1.0.dev0'
+=======
+__version__ = '0.9.2'
+>>>>>>> release v0.9.2
