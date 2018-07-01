eccba172fce03eb47d52b9bdd9986389f3ff1ef2
Nicholas Chammas <nicholas.chammas@gmail.com>
2018-07-01 00:00:02 -0400

    further restrict python-dateutil versions
    For botocore.

--- setup.py
+++ setup.py
@@ -58,12 +58,14 @@ setuptools.setup(
         # indirect dependency of Flintrock, and PyInstaller chokes on
         # dateutil 2.5.0.
         # See: https://github.com/pyinstaller/pyinstaller/issues/1848
-        'python-dateutil == 2.6.1',
+        # More recently, botocore also imposes additional restrictions
+        # on the acceptable versions of python-dateutil.
+        'python-dateutil >= 2.5.3, < 2.7.0',
         # This is to address reports that PyInstaller-packaged versions
         # of Flintrock intermittently fail due to an out-of-date version
         # of Cryptography being used.
         # See: https://github.com/nchammas/flintrock/issues/169
-        'cryptography == 2.6.1',
+        'cryptography >= 1.7.2',
     ],
 
     entry_points={
