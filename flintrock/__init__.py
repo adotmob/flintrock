a780926bed7ad3c3b5cb4fed06f5ded2151e8e45
dm-tran <dm-tran@users.noreply.github.com>
2018-09-14 15:24:31 +0200

    start version 0.9.4.dev

--- flintrock/__init__.py
+++ flintrock/__init__.py
@@ -1,10 +1,2 @@
 # See: https://packaging.python.org/en/latest/distributing/#standards-compliance-for-interoperability
-<<<<<<< HEAD
-<<<<<<< HEAD
 __version__ = '1.1.0.dev0'
-=======
-__version__ = '0.9.2'
->>>>>>> release v0.9.2
-=======
-__version__ = '0.9.3'
->>>>>>> release v0.9.3
